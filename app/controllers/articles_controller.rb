ae22881ec8a3dcabead01586d0a0973bf7207e1b
Muteesa Fred <muteesafred@hotmail.com>
2019-08-30 09:59:34 -0400

    added authentication and comments model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+http_basic_authenticate_with name: "fred", password: "fred123", except: [:index, :show]
+
   def index
 	      @articles = Article.all
   end
